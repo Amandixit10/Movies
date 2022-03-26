a2456531cd8a82a5a7c03f4026478e2ae09afa01
Aman Dixit <amandikshit10@gmail.com>
2022-03-26 15:51:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const API_KEY='api_key=9917b14637d81d7d42dc2c1cfbcac628';
+const BASE_URL='https://api.themoviedb.org/3';
+const   API_URL=BASE_URL+'/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL='https://image.tmdb.org/t/p/w500';
+const main=document.getElementById('main');
+const form=document.getElementById('form');
+const search=document.getElementById('search');
+const ssearchURL=BASE_URL+'/search/movie?'+API_KEY;
+
+
+getMovies(API_URL);
+
+function getMovies(url){
+    fetch(url).then(res=>res.json()).then(data=>{
+showMovies(data.results);
+    
+    })
+function showMovies(data){
+
+main.innerHTML='';
+    data.forEach(movie => {
+        const {title,poster_path,vote_average,overview}=movie;
+        const movieEl=document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>Movie Title</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h1>Overview</h1>
+            ${overview}
+        </div>
+`
+main.appendChild(movieEl);
+    })
+}
+
+function getColor(vote)
+{
+    if(vote>=8)
+    {
+        return "green";
+    }
+    else if(vote>=5)
+    {
+        return "orange";
+    }
+    return "red";
+}
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if(searchTerm){
+        getMovies(ssearchURL+'&query='+searchTerm);
+    }
+    else{
+        getMovies(API_URL);
+    }
+})
+
